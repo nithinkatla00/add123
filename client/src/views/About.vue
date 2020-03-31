20af004db1a7bcf79309f7b112b5eedb988b6b65
katla nithin <nithikatla00@gmail.com>
2020-03-31 20:40:20 +0530

    first commit

---
+++ client/src/views/About.vue
@@ -0,0 +1,81 @@
+<template>
+<div>
+  <section class="site-banner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-md-12 site-title">
+                        <h3 class="title-text">Hey__ _ _ _ _</h3>
+                        <h1 class="title-text text-uppercase">I AM NITHIN KATLA</h1>
+                        <h4 class="title-text text-uppercase">MEVN Stack Developer</h4>
+    
+                    </div>
+                    <div class="col-lg-6 col-md-12 banner-image">
+                        <img src="./banner-image.png" alt="banner-img" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+  </section>
+
+
+<section class="about-area">
+    <div class="container-fluid">
+          <div class="row">
+                    <div class="col-lg-6 col-md-12">
+                        <div class="about-image">
+                            <img src="./p2.jpg" alt="About us" class="img-fluid" style="width:500px;height:500px;">
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12 about-title">
+                        <h2>Let Me Introduce Myself</h2>
+                        <div class="paragraph py-4 w-75">
+                            <h3 class="para">
+                                I am nithin katla.I completed my B.tech from Vardhaman college of engineering.
+                            </h3>
+                            <div class="para">
+                                  Learned MEVN stack Development...Love to DO Coding.
+                                  <h3>skills:</h3>
+                                  <ul>
+                                    <li>programing languages: <h4>C,JAVA,PYTHON</h4></li>
+                                    <li>Technologies:<h4>Mongodb,vuejs,reactjs,Nodejs,Express,MySql</h4></li>
+                                  </ul>                                
+                            </div>
+                        </div>
+                  </div>
+            </div>
+       </div>
+  </section>
+
+
+
+  <div class="card">
+      <div class="card-header">PALISADE</div>
+      <div class="card-body">
+          <h5 class="card-title">you can post anything after login into ur account.</h5>
+          <p class="card-text">HAPPY LEARNING......................</p>
+      </div>
+  </div>
+      <footer class="footer-area">
+        <div class="container">
+            <div class="">
+                <div class="social text-center">
+                    <h5 class="text-uppercase">Follow me</h5>
+                    <a href="#"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a>&nbsp;&nbsp;&nbsp;
+                    <a href="#"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>&nbsp;&nbsp;&nbsp;
+                    <a href="#"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a>&nbsp;&nbsp;&nbsp;
+                </div>
+            </div>
+        </div>
+    </footer>
+    <br><br>
+   <center><div>Done Using MEVN</div></center>
+</div>
+
+</template>
+<script>
+
+</script>
+<style  scoped>
+.para{
+  font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif 
+}
+</style>
---
+++ config/database.js
@@ -0,0 +1,11 @@
+if (process.env.NODE_ENV === 'production') {
+    module.exports = {
+        database: "mongodb+srv://nithinkatla00:katlanithin333@cluster0-dpvaf.mongodb.net/test?retryWrites=true&w=majority",
+        secret: 'yoursecret'
+    };
+}else {
+    module.exports = {
+        database: "mongodb://localhost:27017/meven_auth",
+        secret: 'yoursecret'
+    };
+}
